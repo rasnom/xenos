1823a8de883f6fba696bcd8c3ac4560b68a838b6
Ransom <ransom.byers@gmail.com>
2017-04-16 18:53:10 -0700

    Add items#create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,14 @@ class ItemsController < ApplicationController
 		@item = Item.new
 	end
 
+	def create
+		@new_item = Item.new(item_params)
+		if @new_item.save
+			redirect_to '/items'
+		end
+	end
+
+	def item_params
+		params.require(:item).permit(:name, :ingredients, :description, :price, :category_id)
+	end
 end
