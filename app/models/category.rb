b1731672a8b3dfb148076e6adf96b3ae7f3fb933
Ransom <ransom.byers@gmail.com>
2017-04-16 23:01:11 -0700

    Connect Category and Item

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
 	validates :name, :order, presence: true
 	validates :name, uniqueness: true
 
+	has_many :items
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
 	validates :name, :price, :category_id, presence: true
 	validates :name, uniqueness: true
 
+	belongs_to :category
 end
