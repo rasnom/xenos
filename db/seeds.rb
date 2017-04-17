d713d462819703d650fac104372747df5a1bfa90
Ransom <ransom.byers@gmail.com>
2017-04-16 18:32:44 -0700

    Add a few seed items and display on the items index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sample_starters = [
+	{ name: 'Green Fig Salad', price: 9, category_id: 1 },
+	{ name: 'Sardine Salad', price: 9, category_id: 1 },
+	{ name: 'Stuffed Olives', price: 9, category_id: 1 },
+	{ name: 'Lobster Soup', price: 9, category_id: 1 },
+	{ name: 'Grilled Bull Heart', price: 9, category_id: 1 },
+	{ name: 'Bacon Wrapped Dates', price: 9, category_id: 1 },
+	{ name: 'Grilled Heloumi', price: 9, category_id: 1 },
+	{ name: 'Lamb Chop Lollipops', price: 9, category_id: 1 },
+	{ name: 'Nectarine Carpacicio', price: 9, category_id: 1 },
+	{ name: 'The Pompei Platter', price: 9, category_id: 1 }
+]
+
+Item.create(sample_starters)
